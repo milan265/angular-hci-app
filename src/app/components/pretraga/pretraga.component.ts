1ab99aaf896853f417eb489585173268fa4f7398
milan265 <savicmmilan94@gmail.com>
2020-09-21 15:01:45 +0200

    pretraga

--- src/app/components/pretraga/pretraga.component.ts
+++ src/app/components/pretraga/pretraga.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { Restoran } from 'src/app/models/restoran.model';
 import { RestoranService } from 'src/app/services/restoran.service';
 
 @Component({
@@ -11,15 +12,35 @@ import { RestoranService } from 'src/app/services/restoran.service';
 })
 export class PretragaComponent implements OnInit {
 
+  imaRestorana: boolean = false;
+  restorani: Array<Restoran> = [];
+  email: string = ""
+
   constructor(private titleService: Title, private cookieService: CookieService, private router: Router,
-              private restoranService:RestoranService) { }
+              private restoranService: RestoranService) { }
 
   ngOnInit() {
     if(this.cookieService.get("ulogovan")=="false"){
       this.router.navigate(["404"]);
     }else{
-      this.titleService.setTitle("Dostava hrane");
+      this.titleService.setTitle("Pretraga");
+      this.email = this.cookieService.get("email");
+      this.podaci();
     }
+  }
+
+  podaci():void{
+    this.restorani = this.restoranService.getSviRestorani();
+    this.imaRestorana = this.restorani.length>0? true:false;
+  }
+
+  getOcena(id:number):string{
+    return this.restoranService.getOcenaById(id);
+  }
+
+  prikaziRestoran(id:number):void{
+    this.cookieService.set("restoran_id",id.toString());
+    this.router.navigate(['prikaz-restorana']);
   }
 
 }
